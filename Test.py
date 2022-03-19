2b610c43f6ca87a39353090b611e177dc1580052
DESKTOP-6C0JQAI\dbwjd <dbwjd20111>
2022-03-19 16:34:38 +0900

    python 설치

---
+++ Test.py
@@ -0,0 +1,18 @@
+import turtle 
+
+turtle.showturtle()
+turtle.write("Welcome to Python")
+
+turtle.forward(100)
+turtle.right(90)
+turtle.color("red")
+
+turtle.forward(50)
+turtle.right(90)
+turtle.color("green")
+
+turtle.forward(100)
+turtle.right(45)
+turtle.forward(80)
+
+turtle.done()
