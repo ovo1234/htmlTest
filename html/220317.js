cae4e4dc2dfd4eb1aa8dcd0c6f1125c5a5a89992
DESKTOP-6C0JQAI\dbwjd <dbwjd20111>
2022-03-30 23:47:52 +0900

    ..

---
+++ html/220317.js
@@ -0,0 +1,16 @@
+consloe.log('고급웹프로그래밍 로그');
+
+const btnCalc = document.querySelector('#btnCalc');
+btnCalc.addEventListener("click", function(e){
+    consloe.log("btnCalc clicked!");
+})
+
+// const a = 1;
+// // a = 1
+// a = 2;
+// // error
+
+// let b = 3;
+// // b = 3
+// b = 4;
+// // b = 4
