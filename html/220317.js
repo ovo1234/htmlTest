8a6661dea072dec28e1dfa0828b3c0cb157ad3c0
DESKTOP-6C0JQAI\dbwjd <dbwjd20111>
2022-04-03 19:38:11 +0900

    웹프 공부

--- html/220317.js
+++ html/220317.js
@@ -1,16 +1,44 @@
-consloe.log('고급웹프로그래밍 로그');
+console.log("고급웹프로그래밍 로그");
 
-const btnCalc = document.querySelector('#btnCalc');
-btnCalc.addEventListener("click", function(e){
-    consloe.log("btnCalc clicked!");
-})
+const inputBalance = document.querySelector("#inpBalance");
+const inputRange = document.querySelector("#inpRange");
+const selRangeUnit = document.querySelector("#selRangeUnit");
+const inputInterest = document.querySelector("#inpInterest");
+const btnCalc = document.querySelector("#btnCalc");
+// btnCalc.addEventListener("click", calcSimpleInterest);
+btnCalc.addEventListener("click", onBtnCalculation);
 
-// const a = 1;
-// // a = 1
-// a = 2;
-// // error
+function onBtnCalculation() {
+  const balance = Number(inputBalance.value);
+  const interest = Number(inputInterest.value);
+  const range = Number(inputRange.value);
 
-// let b = 3;
-// // b = 3
-// b = 4;
-// // b = 4
+  // 단리 계산 시 결과
+  const simpleInterestResult = calcSimpleInterest(balance, interest, range);
+  console.log("단리 적용 후 원금변화", simpleInterestResult);
+  // 복리 계산 시 결과
+  const compoundInterestResult = calcCompoundInterest(balance, interest, range);
+  console.log("복리 적용 후 원금변화", compoundInterestResult);
+  // 복리 계산 시 결과
+}
+
+function calcCompoundInterest(balance, interest, range) {
+  // calcSimpleInterest(balance, interest, range);
+  // 복리 계산 예제
+  // 1회 : 100 * 1.1 = 110
+  // 2회 : 110 * 1.1 = 121
+  // 3회 : 121 * 1.1 = Math.floor(133.10000000002)
+
+  for (let i = 0; i < range; i++) {
+    balance += balance * (interest / 100);
+  }
+
+  return Math.floor(balance);
+}
+
+function calcSimpleInterest(balance, interest, range) {
+  // 순수 이자
+  const pureInterest = balance * (interest / 100) * range;
+
+  return balance + pureInterest;
+}
---
+++ html/interestCalculator.js
@@ -0,0 +1,31 @@
+
+// writer : 
+// 복리계산기 함수
+// param : balance(원금), interest(이율), range(횟수)
+// return 복리계산결과
+function calcCompoundInterest(balance, interest, range) {
+    // calcSimpleInterest(balance, interest, range);
+    // 복리 계산 예제
+    // 1회 : 100 * 1.1 = 110
+    // 2회 : 110 * 1.1 = 121
+    // 3회 : 121 * 1.1 = Math.floor(133.10000000002)
+  
+    for (let i = 0; i < range; i++) {
+      balance += balance * (interest / 100);
+    }
+  
+    return Math.floor(balance);
+  }
+  
+
+  // writer : 
+// 복리계산기 함수
+// param : balance(원금), interest(이율), range(횟수)
+// return 단리계산결과
+  function calcSimpleInterest(balance, interest, range) {
+    // 순수 이자
+    const pureInterest = balance * (interest / 100) * range;
+  
+    return balance + pureInterest;
+  }
+  
