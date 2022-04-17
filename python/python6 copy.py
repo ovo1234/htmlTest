740b966c48b94dd0445147b6837d243383a33f3f
DBWJD\dbwjd <dbwjd20111@naver.com>
2022-04-17 13:21:36 +0900

    0417

---
+++ python/python6 copy.py
@@ -0,0 +1,6 @@
+from python6 import gcd
+
+n1 = eval(input("Enter the first integer : "))
+n2 = eval(input("Enter the second` integer : "))
+
+print("The greatest common divisor for", n1, "and", n2, "is", gcd(n1,n2))
