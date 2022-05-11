cc12f519405127e3bc3dc5d31198a34feb436d1a
DESKTOP-0USOF49\dbwjd <dbwjd20111@naver.com>
2022-05-11 10:59:26 +0900

    ..

--- python/python3 copy.py
+++ python/python3 copy.py
@@ -1,21 +1,21 @@
 # 3주차
 
-# num = eval(input("Enter an Integer: "))
+num = eval(input("Enter an Integer: "))
 
-# d1 = num % 10
-# num = num //10
+d1 = num // 10
+num = num %10
 
-# d2 = num % 10
-# num = num //10
+d2 = num // 10
+num = num %10
 
-# d3 = num % 10
-# num = num //10
+d3 = num // 10
+num = num %10
 
-# d4 = num % 10
-# num = num //10
+d4 = num // 10
+num = num %10
 
-# print(d1, end="")
-# print(d2, end="")
-# print(d3, end="")
-# print(d4, end="")
+print(d1, end="")
+print(d2, end="")
+print(d3, end="")
+print(d4, end="")
 
--- python/test.py
+++ python/test.py
@@ -1,3 +1,7 @@
+# print((10.5 + 2 * 3) /(45-3.5))
+
+# print(format(57.467657, '10.2e'))
+
 # str = (input("숫자를 입력하세요."))
 
 # for i in range(1,10) :
@@ -6,5 +10,10 @@
 # for i in range(10,0,-1):
 #     print(i)
 
-for i in range(3):
-    print(i)
+# for i in range(3):
+#     print(i)
+
+import random
+
+lottery = random.randint(0,99)# 0~99 까지 정수형 나옴
+print(lottery)
